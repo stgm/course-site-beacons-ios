1df55c2b4068968364208ec6575977d54504fdf2
Marijn Jansen <therijn@users.noreply.github.com>
2017-10-13 14:48:44 +0200

    Initial Commit

---
+++ iBeacons-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBeacons-Test
+//
+//  Created by Marijn Jansen on 13/10/2017.
+//  Copyright © 2017 Marijn Jansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
